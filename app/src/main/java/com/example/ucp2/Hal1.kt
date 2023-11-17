a3b58f8d7e59a4f0809f1ed0a09de1be5877b54f
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-11-17 15:12:30 +0700

    Fix

--- app/src/main/java/com/example/ucp2/Hal1.kt
+++ app/src/main/java/com/example/ucp2/Hal1.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,12 +14,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Hal1(
     onNextButtonClicked: () -> Unit
 ) {
-    val image = painterResource(id = R.drawable.d95)
+    val image = painterResource(id = R.drawable.umy)
     Column(
         modifier = Modifier,
         verticalArrangement = Arrangement.Center
